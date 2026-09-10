73e0a66c473958fff285e56db3ea021e9f53c9e1
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-09-11 00:17:59 +0900

    chore: use Bun 1.4 in dev shell

--- flake.nix
+++ flake.nix
@@ -21,9 +21,31 @@
 
       perSystem =
         { pkgs, inputs', ... }:
+        let
+          bun = pkgs.bun.overrideAttrs (_: {
+            version = "1.4.2";
+            src = pkgs.fetchurl {
+              url =
+                {
+                  "aarch64-darwin" = "https://github.com/oven-sh/bun/releases/download/bun-v1.4.2/bun-darwin-aarch64.zip";
+                  "aarch64-linux" = "https://github.com/oven-sh/bun/releases/download/bun-v1.4.2/bun-linux-aarch64.zip";
+                  "x86_64-linux" = "https://github.com/oven-sh/bun/releases/download/bun-v1.4.2/bun-linux-x64-baseline.zip";
+                }
+                .${pkgs.stdenv.hostPlatform.system};
+              hash =
+                {
+                  "aarch64-darwin" = "sha256-kJh6OhbX21VtiGrD1VHnttPt8KHPQ6yu1iLoZ2vh0S8=";
+                  "aarch64-linux" = "sha256-VDKLvC2cjgyfiSxUTWbFeoO4QTnjSQnl7oF1jxrI/ac=";
+                  "x86_64-linux" = "sha256-xngEDxT+BEDrg503y9DOTAUaMtpygGrJfeamqra/co8=";
+                }
+                .${pkgs.stdenv.hostPlatform.system};
+            };
+          });
+        in
         {
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
+              bun
               nodejs_24
               pnpm
               inputs'.llm-agents.packages.pi
